97a6b07ba45beb2882f45b80110638e3bf0ce652
Zach <z6chy@icloud.com>
2022-01-30 11:27:19 -0700

    attendee done but not passing tests

--- theTower/server/controllers/AttendeesController.js
+++ theTower/server/controllers/AttendeesController.js
@@ -1,25 +1,50 @@
 import { Auth0Provider } from "@bcwdev/auth0provider";
 import BaseController from "../utils/BaseController";
 import { attendeesService } from "../services/AttendeesService";
+import { towerEventsService } from "../services/TowerEventsService";
 
 
 export class AttendeesController extends BaseController {
     constructor() {
-        super("/api/")
+        super("/a")
         this.router
-            // .get('ccount/attendees', this.getMyAttendance)
-            // .get('events/:id/attendees', this.getEventAttendees)
+            .get('ccount/attendees', this.getMyAttendance)
+            .get('events/:id/attendees', this.getEventAttendees)
             .use(Auth0Provider.getAuthorizedUserInfo)
-            .post('attendees', this.createAttendee)
-        // .delete('attendees/:id', this.removeAttendee)
+            .post('pi/attendees', this.createAttendee)
+            .delete('pi/attendees/:id', this.removeAttendee)
     }
 
+    async getMyAttendance(req, res, next) {
+        try {
+            const myAttendance = await attendeesService.getMyAttendance()
+            return res.send(myAttendance)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async getEventAttendees(req, res, next) {
+        try {
+            const eventAttendee = await attendeesService.getEventAttendance(req.params.id)
+        } catch (error) {
+            next(error)
+        }
+    }
     async createAttendee(req, res, next) {
         try {
-            // req.body.accountId = req.accountId.id
-            req.body.creatorId = req.userInfo.id
+            req.body.accountId = req.userInfo.id
+            req.body.eventId = req.DbContext.TowerEvents.id
             const createdAttendee = await attendeesService.create(req.body)
             return res.send(createdAttendee)
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async removeAttendee(req, res, next) {
+        try {
+            const updated = await attendeesService.remove(req.params.id)
         } catch (error) {
             next(error)
         }
--- theTower/server/services/AttendeesService.js
+++ theTower/server/services/AttendeesService.js
@@ -1,21 +1,38 @@
 import { dbContext } from "../db/DbContext";
 import { BadRequest } from "../utils/Errors";
+import { towerEventsService } from "./TowerEventsService";
 
 
 class AttendeesService {
 
-    async getMyAttendance(id) {
-        const myAttendance = await dbContext.Attendance.findById(id).populate('creator', 'name')
+    async getMyAttendance(accountid) {
+        const myAttendance = await dbContext.Attendance.findById(accountid).populate('creator', 'name')
         if (!myAttendance) {
             throw new BadRequest("Unable to get your events")
         }
         return myAttendance
     }
 
+    async getEventAttendance(eventId) {
+        const eventAttendance = await dbContext.Attendance.find(eventId).populate('creator', 'name')
+        if (!eventAttendance) {
+            throw new BadRequest('Invalid Event')
+        }
+        return eventAttendance
+    }
+
     async create(newAttendance) {
         const createdAttendance = await dbContext.Attendance.create(newAttendance)
         await createdAttendance.populate('creator', 'name picture')
         return createdAttendance
+    }
+
+    async remove(id, userId) {
+        const original = await this.getMyAttendance(id)
+        if (original.creatorId.toString() !== userId) {
+            throw new BadRequest('Unable to Remove')
+        }
+        await dbContext.Attendance.findByIdAndRemove({ _id: id, creatorId: userId })
     }
 }
 
