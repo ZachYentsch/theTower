7e9d83fc71863aaa6c29b2160a9497167be991d5
Zach <z6chy@icloud.com>
2022-03-07 14:36:06 -0700

    goinglive

--- theTower/client/app/env.js
+++
@@ -1,5 +0,0 @@
-export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = 'dev-9kkxugcf.us.auth0.com'
-export const audience = 'https://theTower.com'
-export const clientId = '924V7sN2GGnYtkIHiaxaC0aBBwhA2Zpg'
